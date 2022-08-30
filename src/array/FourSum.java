460284be49e35973aad2ef18b8b411035d90524e
CuveeFer <942122009@qq.com>
2022-08-31 00:06:00 +0800

    FourSum

---
+++ src/array/FourSum.java
@@ -0,0 +1,67 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Fer
+ * date 2022/8/29
+ */
+
+public class FourSum {
+
+    /*
+        排序 + 双指针
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        Arrays.sort(nums);
+        if (len < 4) {
+            return res;
+        }
+        for (int i = 0; i < len - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target) {
+                    continue;
+                }
+                int left = j + 1, right = len - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left+1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right-1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
