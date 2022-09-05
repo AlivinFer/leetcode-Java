0ef7d20fa83ae19bd24bec21d226abdc03aea5ff
CuveeFer <942122009@qq.com>
2022-09-06 00:25:33 +0800

    存在重复元素Ⅱ

---
+++ src/array/ContainsNearbyDuplicate.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author Fer
+ * date 2022/9/5
+ */
+
+public class ContainsNearbyDuplicate {
+
+    /*
+        存在重复元素Ⅱ
+        数组内如果存在重复的元素，满足下标 abs(i-j) <= k
+     */
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
