b0b6e01a53bc78489e82dd9c58be157658d87f96
CuveeFer <942122009@qq.com>
2022-10-24 23:41:05 +0800

    丢失的数字

---
+++ src/array/MissingNumber.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * @author Fer
+ * date 2022/10/24
+ */
+
+public class MissingNumber {
+
+    /*
+        丢失的数字
+     */
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return len;
+    }
+}
