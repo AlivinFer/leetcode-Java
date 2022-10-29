e7044615d3826fcf2e41f12160f85391513bedf9
CuveeFer <942122009@qq.com>
2022-10-29 23:33:10 +0800

    最长连续递增序列

---
+++ src/array/FindLengthOfLCIS.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * @author Fer
+ * date 2022/10/29
+ */
+
+public class FindLengthOfLCIS {
+
+    // 最长连续递增序列
+    public int findLengthOfLCIS(int[] nums) {
+        int max = 1;
+        int len = nums.length;
+        if (len == 1) {
+            return max;
+        }
+        // l 为当前最长连续递增序列
+        int l = 1;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] < nums[i+1]) {
+                l += 1;
+            } else {
+                l = 1;
+            }
+            max = Math.max(max, l);
+        }
+        return max;
+    }
+}
